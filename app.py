bf86758f6ba8e4c1ed679eadaf273f8ac66c0a69
shayliv <shay@bonez.io>
2026-06-25 18:54:08 +0300

    add farewell() in app.py

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ def greet(name: str) -> str:
     return f"hello, {slugify(name)}"
 
 
+def farewell(name: str) -> str:
+    return f"bye, {slugify(name)}"
+
+
 def main() -> None:
     print(greet("World"))
+    print(farewell("World"))
 
 
 if __name__ == "__main__":
